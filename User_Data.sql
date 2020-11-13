2281798ebf2ce2d1d13e3f20bd7bf1791ef01912
bonaventurabs <59414359+bonaventurabs@users.noreply.github.com>
2020-11-13 16:19:17 +0700

    Update User_Data.sql

--- User_Data.sql
+++ User_Data.sql
@@ -5,9 +5,7 @@ create table User_Data (
 	Country VARCHAR(50),
 	Date_of_Birth DATE,
 	Gender VARCHAR(50),
-	PRIMARY KEY(Spotify_username),
-	ON DELETE RESTRICT ON UPDATE CASCADE
-
+	PRIMARY KEY(Spotify_username)
 );
 insert into User_Data (Spotify_username, Real_name, Email_address, Country, Date_of_Birth, Gender) values ('bmardall0', 'Cleavland Kaser', 'ckaser0@ucla.edu', 'Japan', '1994-08-19', 'Male');
 insert into User_Data (Spotify_username, Real_name, Email_address, Country, Date_of_Birth, Gender) values ('kmcgloughlin1', 'Ash Pitbladdo', 'apitbladdo1@tinypic.com', 'Brazil', '1988-12-29', 'Male');

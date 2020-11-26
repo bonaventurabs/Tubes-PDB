f863871af2714a3b816e450fbd507b8c5019554b
bonaventurabs <59414359+bonaventurabs@users.noreply.github.com>
2020-11-26 13:55:41 +0700

    Update Library.sql

--- Library.sql
+++ Library.sql
@@ -2,7 +2,7 @@ create table Library (
 	Spotify_username VARCHAR(16),
 	Track_number INT,
 	Song_ID INT,
-	PRIMARY KEY(Spotify_username,Song_ID),
+	PRIMARY KEY(Spotify_username,Track_number,Song_ID),
 	CONSTRAINT FK_spotify_username_library FOREIGN KEY (Spotify_username) REFERENCES User_Data(Spotify_username),
 	CONSTRAINT FK_song_id_library FOREIGN KEY (Song_ID) REFERENCES Song(Song_ID)
 );
